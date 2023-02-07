3aa48b9a8f9632addbafd8542f7622f6d22e135b
Jairo Leal <jairoandresleal99@gmail.com>
2023-02-07 17:09:19 -0500

    setup catkin_ws with assignment1c pkg

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/jairo/Desktop/AuE8230_Group3/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/jairo/Desktop/AuE8230Spring23_JairoLeal/catkin_ws/devel;/home/jairo/catkin_ws/devel;/home/jairo/Desktop/Autonomy/ROS_workspace/devel;/opt/ros/noetic'.split(';') if '/home/jairo/Desktop/AuE8230Spring23_JairoLeal/catkin_ws/devel;/home/jairo/catkin_ws/devel;/home/jairo/Desktop/Autonomy/ROS_workspace/devel;/opt/ros/noetic' != '' else []

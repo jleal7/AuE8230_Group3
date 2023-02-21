6c7a3a365e9fa2711188b0f16b3c411a8c56f388
Jairo Leal <jairoandresleal99@gmail.com>
2023-02-21 16:09:26 -0500

    addded pkg for assignment 2b

--- catkin_ws/build/CTestTestfile.cmake
+++ catkin_ws/build/CTestTestfile.cmake
@@ -5,7 +5,14 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
+subdirs("assignment2B_turtlebotteleop")
+subdirs("turtlebot3_autorace_2020/turtlebot3_autorace_2020")
+subdirs("turtlebot3_autorace_2020/turtlebot3_autorace_core")
+subdirs("turtlebot3_autorace_2020/turtlebot3_autorace_msgs")
+subdirs("turtlebot3_autorace_2020/turtlebot3_autorace_driving")
 subdirs("turtlebot3_simulations/turtlebot3_simulations")
 subdirs("assignment1c_turtlebot3")
+subdirs("turtlebot3_autorace_2020/turtlebot3_autorace_camera")
+subdirs("turtlebot3_autorace_2020/turtlebot3_autorace_detect")
 subdirs("turtlebot3_simulations/turtlebot3_fake")
 subdirs("turtlebot3_simulations/turtlebot3_gazebo")
---
+++ catkin_ws/build/assignment2B_turtlebotteleop/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jairo/Desktop/AuE8230_Group3/catkin_ws/src/assignment2B_turtlebotteleop
+# Build directory: /home/jairo/Desktop/AuE8230_Group3/catkin_ws/build/assignment2B_turtlebotteleop
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

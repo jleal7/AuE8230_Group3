d4526a5439322d9444887dc037402b95a66e0c57
Jairo Leal <jairoandresleal99@gmail.com>
2023-03-02 20:48:22 -0500

    updating wallFollow.py

---
+++ catkin_ws/src/assignment3a_wallfollowingandobstacleavoidance/scripts/lidarVisualizer_failed.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+#import matplotlib
+#matplotlib.use('Qt4Agg')
+import matplotlib.pyplot as plt
+import rospy
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan #message type used by /scan topic for laser scan data
+import math #so I can use pi, cos and sin
+
+#fuction to run everytime scan data is recieved
+def callback(msg):
+	#plt.cla()
+	#ax.scatter(range(0,360), msg.ranges)
+	#plt.show(block=False)
+	#plt.draw()
+
+	#convert polar coordinates r and theta to x and y so can plot with matplotlib
+	#for ind,value in enumerate(msg.ranges):
+	#	theta = ind * (math.pi/180); #index is angle in degrees, need to convert to rad
+	#	x = value*math.cos(theta) #r is value (value inside current index of ranges)
+	#	y = value*math.sin(theta)
+	#	plt.scatter(x,y)
+	#	#plt.scatter(theta,value)
+	
+	#plt.pause(.1)
+	pass
+
+if __name__ == '__main__':
+	try:
+		#fig = plt.figure()
+		#ax = fig.add_subplot(projection='polar')
+		#ax.set_rmax(10)
+		#ax.grid(True)
+		#plt.show(block=False) 
+		#plt.draw()
+
+		#can only call init_node once in a python script! launch file only created one
+		rospy.init_node('turtlesim_controller',anonymous=True)
+		#our one node will both subscribe to /scan and publish to /cmd_vel
+		
+		#publisher:
+		publisher = rospy.Publisher('/cmd_vel',Twist,queue_size=10)
+		
+		vel = Twist() #move forward at a velocity of 1 and rotate at 0.5 rad/s
+		rate = rospy.Rate(10)
+		
+		#subscriber:
+		subscriber = rospy.Subscriber("/scan", LaserScan, callback)
+		#rospy.spin()
+
+		while not rospy.is_shutdown():
+			#slow settings:
+			vel.linear.x = .2
+			vel.linear.y = 0
+			vel.linear.z = 0
+			vel.angular.x = 0
+			vel.angular.y = 0
+			vel.angular.z = 0
+			
+			publisher.publish(vel)
+			
+			rate.sleep()
+		
+	except rospy.ROSInterruptException:
+		pass
--- catkin_ws/src/assignment3a_wallfollowingandobstacleavoidance/scripts/wallFollow.py
+++ catkin_ws/src/assignment3a_wallfollowingandobstacleavoidance/scripts/wallFollow.py
@@ -1,44 +1,58 @@
 #!/usr/bin/env python3
 
+import matplotlib.pyplot as plt
 import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan #message type used by /scan topic for laser scan data
-import matplotlib.pyplot as plt
 import math #so I can use pi, cos and sin
+import numpy as np
 
-fig = plt.figure()
-ax = fig.add_subplot(projection='polar')
-ax.set_rmax(10)
-ax.grid(True)
-plt.show(block=False)
-
+#function that looks at all 
+def groupFurthest(ranges):
+	#don't assume that INF is always present
+	
+	#need to find groups of INF and groups of real values
+	
+	
 #fuction to run everytime scan data is recieved
 def callback(msg):
-	plt.cla()
+	#any range further than the max range of the lidar will get returned as INF.
+	#so can't just find the furthest away point (np.argmax(ranges)) since there will be a group of INFs and need to take middle ones
 
-	#convert polar coordinates r and theta to x and y so can plot with matplotlib
-	#for ind,value in enumerate(msg.ranges):
-	#	theta = ind * (math.pi/180); #index is angle in degrees, need to convert to rad
-	#	x = value*math.cos(theta) #r is value (value inside current index of ranges)
-	#	y = value*math.sin(theta)
-	#	plt.scatter(x,y)
-	#	#plt.scatter(theta,value)
+	#IND IS NOT RETURNING FURTHEST DIRECTION!!
+	ind = np.argmax(msg.ranges) #returns index of greatest value in np array
+	print(ind)
 	
-	ax.scatter(range(0,360), msg.ranges)
-		
-	plt.show(block=False)
-	plt.pause(.1)
+	#we want the greatest ind to always be 0 (robot is travling towards a clear path)
+	#NEED TO TURN BASED ON HOW OFF FAR FROM 0 ind IS
+	#IF IND > 0, POSITIVE TURN
+	while ind != 0:
+		print("turning")
+		print(ind)
+		#stop moving and turn until it is
+		vel.linear.x = 0.2
+		vel.linear.y = 0
+		vel.linear.z = 0
+		vel.angular.x = 0
+		vel.angular.y = 0
+		vel.angular.z = ind/9
+			
+		publisher.publish(vel)
+			
+		rate.sleep()
+	#rospy.loginfo(type(msg.ranges))
+	#rospy.loginfo(max(msg.ranges))
+	
 
 if __name__ == '__main__':
-	try:		
-		#can only call init_node once in a python script! launch file only created one
+	try:
+		#can only call init_node once in a python script! (launch file only created one node)
 		rospy.init_node('turtlesim_controller',anonymous=True)
 		#our one node will both subscribe to /scan and publish to /cmd_vel
 		
 		#publisher:
 		publisher = rospy.Publisher('/cmd_vel',Twist,queue_size=10)
-		
-		vel = Twist() #move forward at a velocity of 1 and rotate at 0.5 rad/s
+		vel = Twist() #create Twist message variable to write velocity in and publish
 		rate = rospy.Rate(10)
 		
 		#subscriber:
@@ -46,8 +60,8 @@ if __name__ == '__main__':
 		#rospy.spin()
 
 		while not rospy.is_shutdown():
-			#slow settings:
-			vel.linear.x = .2
+			print("straight")
+			vel.linear.x = 0.2
 			vel.linear.y = 0
 			vel.linear.z = 0
 			vel.angular.x = 0
